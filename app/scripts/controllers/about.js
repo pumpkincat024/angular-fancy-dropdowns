2f3fb412b6461a29e37bb5dae7ed882a45ad1872
Kristy Carrion <kicarrion1@catamount.wcu.edu>
2016-04-23 11:52:06 -0400

    Yoeman scaffolding for the app.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularFancyDropdownsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularFancyDropdownsApp
+ */
+angular.module('angularFancyDropdownsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
